64f06ba8962142a051ba94210fd98be7b23efcc7
yiqiu <yiqiu2333@outlook.com>
2025-04-18 16:54:20 +0800

    聊天UI添加标题

--- BooomProject/Assets/Scripts/UI/PhoneChatUI.cs
+++ BooomProject/Assets/Scripts/UI/PhoneChatUI.cs
@@ -4,82 +4,73 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-public class PhoneChatUI : MonoBehaviour
-{
+public class PhoneChatUI : MonoBehaviour {
     [SerializeField] private Transform buttomButton;
-    //public event EventHandler OnChatWindowShow;
+    [SerializeField] private TextMeshProUGUI chatTitle;
+
+    public event EventHandler OnChatWindowShow;
     public Button backBtn;
     public Text title;
     public TMP_InputField inputField;
     public Transform itemsParent;
     public ScrollRect scrollRect;
-    private void Awake()
-    {
-        backBtn.onClick.AddListener(() =>
-        {
+    private void Awake() {
+        backBtn.onClick.AddListener(() => {
             HideMe();
         });
-        inputField.onSubmit.AddListener((message) =>
-        {
+        inputField.onSubmit.AddListener((message) => {
             OnSubmit(message);
             inputField.text = string.Empty;
         });
     }
-    public void CreateChat()
-    {
+    public void CreateChat() {
         // 创建历史对话
-        foreach (ChatFragment fragment in ChatWindowManager.Instance.history)
-        {
+        foreach (ChatFragment fragment in ChatWindowManager.Instance.history) {
             GameObject obj;
             if (fragment.left) obj = Instantiate(ChatWindowManager.Instance.leftItem, itemsParent);
             else obj = Instantiate(ChatWindowManager.Instance.rightItem, itemsParent);
             obj.GetComponent<ChatItemUI>().init(fragment);
+            chatTitle.text = obj.GetComponent<ChatItemUI>().chatTitle;
         }
     }
 
-    public void UpdateContent()
-    {
+    public void UpdateContent() {
         // 清空对话
-        for (int i = 0; i < itemsParent.childCount; i++)
-        {
+        for (int i = 0; i < itemsParent.childCount; i++) {
             Destroy(itemsParent.GetChild(i).gameObject);
         }
         CreateChat();
     }
     // 发送信息
-    public void OnSubmit(string content)
-    {
+    public void OnSubmit(string content) {
         DateTime NowTime = DateTime.Now.ToLocalTime();
         // 将时间格式化
         string currentTime = NowTime.ToString("HH:mm");
         GameObject obj = Instantiate(ChatWindowManager.Instance.rightItem, itemsParent);
         print(content);
-        ChatFragment newChat = new ChatFragment(currentTime, ChatWindowManager.Instance.ownerIcon, content, false);
+        ChatFragment newChat = new ChatFragment(currentTime, ChatWindowManager.Instance.ownerIcon, content, false, "Test");
         obj.GetComponent<ChatItemUI>().init(newChat);
         // 添加到历史
         ChatWindowManager.Instance.history.Add(newChat);
         scrollRect.verticalNormalizedPosition = 0f;
     }
 
-   // 联系顾客按钮监听事件
-   // 测试方法
-    public void OnButtonClick()
-    {
+    // 联系顾客按钮监听事件
+    // 测试方法
+    public void OnButtonClick() {
         // 传入history
         // 传入信息
         ShowMe();
         UpdateContent();
-       
+
     }
 
-    public void ShowMe()
-    {
+    public void ShowMe() {
         gameObject.SetActive(true);
         UpdateContent();
     }
 
-    public void HideMe()
-    {
+    public void HideMe() {
         gameObject.SetActive(false);
     }
 }
--- BooomProject/Assets/Scripts/Utilities/DataCollections.cs
+++ BooomProject/Assets/Scripts/Utilities/DataCollections.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class ChatFragment
-{
+public class ChatFragment {
+    public string chatTitle;
     public string time;
     public Sprite icon;
     public string content;
     public bool left;
-    public ChatFragment(string time, Sprite icon, string content,bool left)
-    {
+
+    public ChatFragment(string time, Sprite icon, string content, bool left, string chatTitle) {
         this.time = time;
         this.icon = icon;
         this.content = content;
         this.left = left;
+        this.chatTitle = chatTitle;
     }
 }
 
