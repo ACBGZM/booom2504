1c39f4240c05b2b0cefda16e22d288ad06287560
yiqiu <yiqiu2333@outlook.com>
2025-04-23 19:26:56 +0800

    接单后显示在我的订单

--- BooomProject/Assets/Scripts/Player/PlayerAnimator.cs
+++ BooomProject/Assets/Scripts/Player/PlayerAnimator.cs
@@ -6,14 +6,12 @@ public class PlayerAnimator : MonoBehaviour
 {
     [SerializeField] public PlayerController _playerController;
 
-    private const string ANIMATOR_TRIGGER_IS_WALKING = "IsWalking";
-
     private Animator _animator;
 
     private void Awake() => _animator = GetComponent<Animator>();
 
     private void Update() {
-        _animator.SetBool(ANIMATOR_TRIGGER_IS_WALKING, _playerController.IsWalking());
+        _animator.SetBool(GameplaySettings.ANIMATOR_BOOL_IS_WALKING, _playerController.IsWalking());
     }
 
 }
