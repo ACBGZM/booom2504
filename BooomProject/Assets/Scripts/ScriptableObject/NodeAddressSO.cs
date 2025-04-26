9516271ec6a9c45c571c4bfc2697f63da94f5f9e
miu <acbgzm@126.com>
2025-04-26 21:33:20 +0800

    删除NodeAddressSO

--- BooomProject/Assets/Scripts/ScriptableObject/NodeAddressSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-// 节点地址映射数据容器
-[CreateAssetMenu(fileName = "NodeAddress", menuName = "NodeAddress/NodeAddressSO")]
-public class NodeAddressSO : ScriptableObject
-{
-    // 键 地址名  值 节点编号
-    public SerializableDictionary<string, int> nodeAddress;
-}
