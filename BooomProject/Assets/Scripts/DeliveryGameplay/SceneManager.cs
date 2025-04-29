42f422444fdf35560fa1e93b0808ba97e403c347
miu <acbgzm@126.com>
2025-04-29 21:41:32 +0800

    修复load报错，删除loading panel等老的load async相关代码和资产

--- BooomProject/Assets/Scripts/DeliveryGameplay/SceneManager.cs
+++ BooomProject/Assets/Scripts/DeliveryGameplay/SceneManager.cs
@@ -1,81 +1,48 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
+using DG.Tweening;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameSceneManager : MonoBehaviour {
-    [SerializeField] private GameObject _loadingPanel;
-    [SerializeField] private Slider _progressBar;
-    [SerializeField] private TMP_Text _progressText;
     [SerializeField] private List<GameObject> _disableWhileLoading;
-
-    // 场景切换
-    private SceneLoader _sceneLoader;
-
-    private void Start()
-    {
-        _sceneLoader = GameObject.Find("SceneLoader").GetComponent<SceneLoader>();
-    }
 
     public void LoadSync(string target_scene) {
         HandleSceneTransition(() => SceneManager.LoadScene(target_scene));
-        StartCoroutine(SceneLoader(target_scene));
+    }
+
+    public void Start()
+    {
+        _fadeImage.color = Color.black;
+        _fadeImage.DOFade(0f, _fadeInDuration).SetEase(Ease.Linear);
     }
 
     // 异步加载场景动画
-    #region 
-    IEnumerator SceneLoader(string target_scene)
+    [SerializeField] Image _fadeImage;
+    private float _fadeOutDuration = 0.75f;
+    private float _fadeInDuration = 1.25f;
+    public void LoadAsyncWithFading(string targetScene)
     {
-        // 设置动画机状态
-        _sceneLoader.sceneAnimator.SetBool("FadeIn", true);
-        _sceneLoader.sceneAnimator.SetBool("FadeOut", false);
-
-        yield return new WaitForSeconds((float)0.75);
-
-        AsyncOperation async = SceneManager.LoadSceneAsync(target_scene);
-        async.completed += OnLoadScene;
+        StartCoroutine(LoadAsyncWithFadingOut(targetScene));
     }
 
-    private void OnLoadScene(AsyncOperation operation)
+    private IEnumerator LoadAsyncWithFadingOut(string targetScene)
     {
-        // 设置动画机状态
-        _sceneLoader.sceneAnimator.SetBool("FadeIn", false);
-        _sceneLoader.sceneAnimator.SetBool("FadeOut", true);
-    }
-    #endregion
-
-
-    public void LoadAsync(string target_scene) {
-        StartCoroutine(LoadSceneAsync(target_scene));
-    }
-
-    private IEnumerator LoadSceneAsync(string target_scene) {
         HandleSceneTransition();
-        _loadingPanel.SetActive(true);
+        _fadeImage.DOFade(1f, _fadeOutDuration).SetEase(Ease.Linear);
+        yield return YieldHelper.WaitForSeconds(_fadeOutDuration);
 
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(target_scene);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(targetScene);
         asyncLoad.allowSceneActivation = false;
 
-        float fakeProgress = 0;
-        while (!asyncLoad.isDone) {
-            fakeProgress = UpdateProgressUI(asyncLoad, fakeProgress);
-
-            if (fakeProgress >= 1f && asyncLoad.progress >= 0.9f) {
-                asyncLoad.allowSceneActivation = true;
-            }
-
+        while (asyncLoad.progress < 0.9f)
+        {
             yield return null;
         }
 
-        // 场景加载完成后，清理加载界面
-        _loadingPanel.SetActive(false);
-        foreach (GameObject panel in _disableWhileLoading) {
-            panel.SetActive(true);
-        }
+        asyncLoad.allowSceneActivation = true;
     }
 
     private void HandleSceneTransition(System.Action loadAction = null) {
@@ -84,16 +51,6 @@ public class GameSceneManager : MonoBehaviour {
         }
 
         //loadAction?.Invoke();
-    }
-
-    private float UpdateProgressUI(AsyncOperation operation, float fakeProgress) {
-        fakeProgress += Time.deltaTime * 0.3f;
-        float progress = Mathf.Clamp01(operation.progress / 0.9f);
-
-        _progressText.text = $"{fakeProgress:P0}";
-        _progressBar.value = fakeProgress;
-
-        return Mathf.Min(fakeProgress, 1f);
     }
 
     public void QuitGame()
